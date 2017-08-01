d08493629fc3060ebdf8ce0a43c41acd6c167b10
pzmi <p.zmigrodzki@gmail.com>
2017-08-01 23:00:56 +0200

    Consuming messages with Spring Cloud Streams Kafka

--- src/main/java/io/github/pzmi/startingwithspring/Controller.java
+++ src/main/java/io/github/pzmi/startingwithspring/Controller.java
@@ -1,10 +1,14 @@
 package io.github.pzmi.startingwithspring;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
+@Slf4j
 public class Controller {
     private String name = "everybody";
 
@@ -16,5 +20,11 @@ public class Controller {
     @GetMapping("/{name}")
     public GreetingName greetWithName(@PathVariable String name) {
         return new GreetingName(name);
+    }
+
+    @StreamListener(Sink.INPUT)
+    public void onMessage(String name) {
+        log.info("Received name: {}", name);
+        this.name = name;
     }
 }
